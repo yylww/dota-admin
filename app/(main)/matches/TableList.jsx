7808a01795dd63920e326ffcd5cbc6175aeb3755
YUYi <505063673@qq.com>
2024-03-16 16:11:30 +0800

    match 可编辑表格

--- app/(main)/matches/TableList.jsx
+++ app/(main)/matches/TableList.jsx
@@ -1,9 +1,63 @@
-import { Table, Space } from "antd"
+import React, { useContext, useEffect, useRef, useState } from "react"
+import { Table, Space, Form, DatePicker } from "antd"
 import dayjs from "dayjs"
 import Link from "next/link"
+import { updateMatch } from "@/app/api/match"
+import { useSWRConfig } from "swr"
+
+const EditableContext = React.createContext(null)
+const EditeableRow = ({ index, ...props }) => {
+  const [form] = Form.useForm()
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  )
+}
+
+const EditableCell = ({ title, editable, children, dataIndex, record, handleSave, ...restProps }) => {
+  const [editing, setEditing] = useState(false)
+  const inputRef = useRef(null)
+  const form = useContext(EditableContext)
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus()
+    }
+  }, [editing])
+  const toggleEdit = () => {
+    setEditing(!editing)
+    form.setFieldsValue({
+      [dataIndex]: dayjs(record[dataIndex]),
+    })
+  }
+  const save = async (id) => {
+    try {
+      const values = await form.validateFields()
+      toggleEdit()
+      handleSave(id, values)
+    } catch (error) {
+      console.log('Save failed: ', error)
+    }
+  }
+  let childNode = children
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item style={{ margin: 0 }} name={dataIndex}>
+        <DatePicker showTime format="YYYY-MM-DD HH:mm" ref={inputRef} onOk={() => save(record.id)} />
+      </Form.Item>
+    ) : (
+      <div onClick={toggleEdit}>
+        { children }
+      </div>
+    )
+  }
+  return <td {...restProps}>{ childNode }</td>
+}
 
 export const TableList = ({data, onDelete}) => {
-  const columns = [
+  const defaultColumns = [
     { title: 'ID', dataIndex: 'id' },
     { 
       title: '赛事', 
@@ -18,7 +72,9 @@ export const TableList = ({data, onDelete}) => {
     { 
       title: '开始时间',
       key: 'startTime',
-      render: (_, record) => <span>{ dayjs(record.startTime).format('YYYY-MM-DD HH:mm') }</span>
+      dataIndex: 'startTime',
+      render: (_, record) => <span>{ dayjs(record.startTime).format('YYYY-MM-DD HH:mm') }</span>,
+      editable: true,
     },
     // { 
     //   title: '比分',
@@ -65,9 +121,36 @@ export const TableList = ({data, onDelete}) => {
       )
     }
   ]
+  const { mutate } = useSWRConfig()
+  const handleSave = async (id, values) => {
+    await mutate(['match'], () => updateMatch(id, values))
+    mutate(key => Array.isArray(key) && key[0] === 'match')
+  }
+  const components = {
+    body: {
+      row: EditeableRow,
+      cell: EditableCell,
+    },
+  }
+  const columns = defaultColumns.map((col) => {
+    if (!col.editable) {
+      return col
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      })
+    }
+  })
   return (
     <Table 
       rowKey="id" 
+      components={components}
       dataSource={data} 
       columns={columns} 
       size="small" 
