15890c79e05d6aaf5545bfe40240c861de2534d7
yylww <505063673@qq.com>
2024-04-22 11:27:29 +0800

    优化

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -12,64 +12,68 @@ export default async function Page() {
   const concluded = await getMatches({ status: 2, take: 10 })
   return (
     <div className="flex flex-col p-4 bg-gray-500 text-gray-100">
-      <div>赛事预告</div>
-      {
-        upcoming && upcoming.length > 0 ?
-        upcoming.map((item, i) => {
-          const { startTime, homeTeam, awayTeam, homeScore, awayScore } = item
-          return (
-            <div className="flex item-center w-full border-b border-b-gray-400 p-2 gap-4" key={i}>
-              <div className="flex items-center justify-center w-40">{ dayjs(startTime).format('MM-DD HH:mm')}</div>
-              <div className="flex flex-1 flex-col justify-between gap-1">
-                <div className="flex items-center justify-between gap-4">
-                  <Image src={homeTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={homeTeam.tag} />
-                  <span className="flex-1">{homeTeam.name}</span>
-                  <span>{homeScore}</span>
+      <div className="mb-4">赛事预告</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        {
+          upcoming && upcoming.length > 0 ?
+          upcoming.map((item, i) => {
+            const { startTime, homeTeam, awayTeam, homeScore, awayScore } = item
+            return (
+              <div className="flex item-center w-full border border-gray-400 p-2 px-4 gap-4 md:gap-6 lg:gap-10" key={i}>
+                <div className="flex items-center justify-center">{ dayjs(startTime).format('MM-DD HH:mm')}</div>
+                <div className="flex flex-1 flex-col justify-between gap-1">
+                  <div className="flex items-center justify-between gap-4">
+                    <Image src={homeTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={homeTeam.tag} />
+                    <span className="flex-1">{homeTeam.name}</span>
+                    <span>{homeScore}</span>
+                  </div>
+                  <div className="flex items-center justify-between gap-4">
+                    <Image src={awayTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={awayTeam.tag} />
+                    <span className="flex-1">{awayTeam.name}</span>
+                    <span>{awayScore}</span>
+                  </div>
                 </div>
-                <div className="flex items-center justify-between gap-4">
-                  <Image src={awayTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={awayTeam.tag} />
-                  <span className="flex-1">{awayTeam.name}</span>
-                  <span>{awayScore}</span>
+                <div className="flex items-center justify-center">
+                  <Link href="/main/battle">
+                    {/* <button className="px-4 py-2 rounded-md bg-gray-900 text-sm">交手记录</button> */}
+                  </Link>
                 </div>
               </div>
-              <div className="flex items-center justify-center w-40">
-                <Link href="/main/battle">
-                  <button className="px-4 py-1 rounded-full bg-blue-500 text-white">交手记录</button>
-                </Link>
-              </div>
-            </div>
-          )
-        }) : <div className="flex justify-center items-center w-full h-16 border-b border-b-gray-400">暂无比赛</div>
-      }
+            )
+          }) : <div className="flex justify-center items-center w-full h-16 border border-gray-400">暂无比赛</div>
+        }
+      </div>
       <div className="my-4">正在进行</div>
-      {
-        ongoing && ongoing.length > 0 ?
-        ongoing.map((item, i) => {
-          const { homeTeam, awayTeam, homeScore, awayScore } = item
-          return (
-            <div className="flex item-center w-full border-b border-b-gray-400 p-2 gap-4" key={i}>
-              <div className="flex items-center justify-center w-40">进行中</div>
-              <div className="flex flex-1 flex-col justify-between gap-1">
-                <div className="flex items-center justify-between gap-4">
-                  <Image src={homeTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={homeTeam.tag} />
-                  <span className="flex-1">{homeTeam.name}</span>
-                  <span>{homeScore}</span>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        {
+          ongoing && ongoing.length > 0 ?
+          ongoing.map((item, i) => {
+            const { homeTeam, awayTeam, homeScore, awayScore } = item
+            return (
+              <div className="flex item-center w-full border border-gray-400 p-2 gap-4 md:gap-6 lg:gap-10" key={i}>
+                <div className="flex items-center justify-center w-40">进行中</div>
+                <div className="flex flex-1 flex-col justify-between gap-1">
+                  <div className="flex items-center justify-between gap-4">
+                    <Image src={homeTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={homeTeam.tag} />
+                    <span className="flex-1">{homeTeam.name}</span>
+                    <span>{homeScore}</span>
+                  </div>
+                  <div className="flex items-center justify-between gap-4">
+                    <Image src={awayTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={awayTeam.tag} />
+                    <span className="flex-1">{awayTeam.name}</span>
+                    <span>{awayScore}</span>
+                  </div>
                 </div>
-                <div className="flex items-center justify-between gap-4">
-                  <Image src={awayTeam.logo} width={0} height={0} sizes="100%" className="w-5 h-auto" alt={awayTeam.tag} />
-                  <span className="flex-1">{awayTeam.name}</span>
-                  <span>{awayScore}</span>
-                </div>
+                {/* <div className="flex items-center justify-center w-40">
+                  <Link href="/main/battle">
+                    <button className="px-4 py-1 rounded-full bg-blue-500 text-white">交手记录</button>
+                  </Link>
+                </div> */}
               </div>
-              <div className="flex items-center justify-center w-40">
-                <Link href="/main/battle">
-                  <button className="px-4 py-1 rounded-full bg-blue-500 text-white">交手记录</button>
-                </Link>
-              </div>
-            </div>
-          )
-        }) : <div className="flex justify-center items-center w-full h-16 border-b border-b-gray-400">暂无比赛</div>
-      }
+            )
+          }) : <div className="flex justify-center items-center w-full h-16 border border-gray-400">暂无比赛</div>
+        }
+      </div>
       <div className="my-4">最近结束</div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
         {
@@ -92,12 +96,12 @@ export default async function Page() {
                 </div>
                 <div className="flex items-center justify-center md:px-4">
                   <Link href={`/main/matches/${id}`}>
-                    <button className="px-4 py-2 rounded-md bg-gray-900 text-white text-sm">比赛数据</button>
+                    <button className="px-4 py-2 rounded-md bg-gray-900 text-sm">比赛数据</button>
                   </Link>
                 </div>
               </div>
             )
-          }) : <div className="flex justify-center items-center w-full h-16 border-b border-b-gray-400">暂无比赛</div>
+          }) : <div className="flex justify-center items-center w-full h-16 border border-gray-400">暂无比赛</div>
         }
       </div>
     </div>
