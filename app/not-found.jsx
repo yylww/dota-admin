15c4f11d82de47f8edfb216c2ffc534b533fa7b0
yylww <505063673@qq.com>
2024-04-24 14:58:28 +0800

    优化 & fix bug

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -4,7 +4,6 @@ export default function NotFound() {
   return (
     <div className='flex flex-col justify-center gap-6 mt-6 text-center'>
       <h2>未找到相关页面</h2>
-      {/* <p>Could not find requested resource</p> */}
       <Link href="/main" className='text-blue-500'>返回首页</Link>
     </div>
   )
