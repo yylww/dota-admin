7b928578644e9025e2726654ad06780bdfca45b6
yylww <505063673@qq.com>
2024-04-28 11:32:27 +0800

    fix bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
           {children}
         </AntdRegistry>
       </body>
-      <GoogleAnalytics gaId='GTM-MVT3BPKN' />
+      <GoogleAnalytics gaId='G-VJSZWKJG6X' />
     </html>
   )
 }
