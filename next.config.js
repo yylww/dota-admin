ac0ce603eda94224b9abdf40f586224dd41803c5
yylww <505063673@qq.com>
2024-04-30 16:31:54 +0800

    性能优化

--- next.config.js
+++ next.config.js
@@ -3,9 +3,15 @@ const createNextIntlPlugin = require('next-intl/plugin')
 const withNextIntl = createNextIntlPlugin(
   './app/lib/i18n.js'
 )
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')()
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  productionBrowserSourceMaps: false,
+  experimental: {
+    serverSourceMaps: false,
+  }
+}
  
-module.exports = withNextIntl(nextConfig)
-// module.exports = nextConfig
+module.exports = process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : withNextIntl(nextConfig)
