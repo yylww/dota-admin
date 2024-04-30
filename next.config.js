d9ebad1820ad8cf2ecdc5a6bc9cb1e7490cd1b8b
yylww <505063673@qq.com>
2024-04-30 17:17:37 +0800

    性能优化

--- next.config.js
+++ next.config.js
@@ -11,7 +11,20 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   experimental: {
     serverSourceMaps: false,
-  }
+  },
+  webpack: (
+    config,
+    { dev },
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+      config.cache.maxMemoryGenerations = 0
+    }
+    // Important: return the modified config
+    return config
+  },
 }
  
 module.exports = process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : withNextIntl(nextConfig)
