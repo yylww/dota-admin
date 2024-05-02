0f9fa35da4ef3cba10380ce2dccaa57d38f5a1b0
yylww <505063673@qq.com>
2024-05-02 15:37:35 +0800

    数据统计页面移动端优化

--- app/[locale]/main/statistics/page.jsx
+++ app/[locale]/main/statistics/page.jsx
@@ -3,12 +3,12 @@ import Ranking from "./Ranking"
 
 export default async function Page() {
   const { banRanking, pickRanking, rateRanking, noplays } = await getStatistics()
-  const banSort = banRanking.sort((a, b) => b.count - a.count).slice(0, 10)
-  const pickSort = pickRanking.sort((a, b) => b.count - a.count).slice(0, 10)
+  const banSort = banRanking.sort((a, b) => b.count - a.count).slice(0, 20)
+  const pickSort = pickRanking.sort((a, b) => b.count - a.count).slice(0, 20)
   const winSort = rateRanking.filter(item => item.count > 3).sort((a, b) => b.percent - a.percent).slice(0, 20)
   const loseSort = rateRanking.filter(item => item.count > 3).sort((a, b) => a.percent - b.percent).slice(0, 20)
   return (
-    <div className="grid grid-cols-2 gap-10 p-4 bg-white">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-4 bg-white">
       <Ranking data={pickSort}>
         <div className="text-base">出场最多的英雄</div>
       </Ranking>
