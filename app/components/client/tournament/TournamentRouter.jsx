ffdf373e2dd9d15c8b02b3875baa3af9746a0008
yylww <505063673@qq.com>
2024-05-20 15:56:41 +0800

    赛事导航优化

--- app/components/client/tournament/TournamentRouter.jsx
+++ app/components/client/tournament/TournamentRouter.jsx
@@ -1,4 +1,4 @@
-import { getLatestTournamentId, getTournament } from "@/app/lib/tournament";
+import { getTournaments } from "@/app/lib/tournament";
 import { useLocale } from "next-intl";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
@@ -6,24 +6,25 @@ import Link from "next/link";
 export default async function TournamentRouter({ current }) {
   const locale = useLocale()
   const t = await getTranslations('tips')
-  const latestTournamentId = await getLatestTournamentId()
-  const prevTournament = current < latestTournamentId ? await getTournament(current + 1) : null
-  const nextTournament = current > 1 ? await getTournament(current - 1) : null
+  const tournaments = await getTournaments()
+  const currentIndex = tournaments.findIndex((item) => item.id === current)
+  const prevTournament = currentIndex > 0 ? tournaments[currentIndex - 1] : null
+  const nextTournament = currentIndex < (tournaments.length - 1) ? tournaments[currentIndex + 1] : null
   return (
     <div className="flex flex-col gap-2 p-2 bg-white">
       {
-        latestTournamentId !== current ?
+        currentIndex !== 0 ?
         <div>
           <span>{ t('previous') }</span>
-          <Link className="text-blue-500" href={`?tournament=${current+1}`}>{ locale === 'en' ? prevTournament.title_en : prevTournament.title }</Link>
+          <Link className="text-blue-500" href={`?tournament=${tournaments[currentIndex-1].id}`}>{ locale === 'en' ? prevTournament.title_en : prevTournament.title }</Link>
         </div> :
         <span></span>
       }
       {
-        current > 1 ?
+        (currentIndex < tournaments.length - 1) ?
         <div>
           <span>{ t('next') }</span>
-          <Link className="text-blue-500" href={`?tournament=${current-1}`}>{ locale === 'en' ? nextTournament.title_en : nextTournament.title }</Link>
+          <Link className="text-blue-500" href={`?tournament=${tournaments[currentIndex+1].id}`}>{ locale === 'en' ? nextTournament.title_en : nextTournament.title }</Link>
         </div> :
         <span></span>
       }
