96d93beb22c92ac117dae75bf68b35624fa2f25e
Kvo <505063673@qq.com>
2025-04-07 10:12:02 +0800

    domain

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,68 +1,68 @@
-import { getMatches } from "./lib/match"
-import { getTournaments } from "./lib/tournament"
+import { getMatches } from './lib/match';
+import { getTournaments } from './lib/tournament';
 
 export default async function sitemap() {
-  const matchData = getMatches()
-  const tournamentData = getTournaments()
-  const [matches, tournaments] = await Promise.all([matchData, tournamentData])
-  const matchesMap = matches.map(item => ({
-    url: `https://www.playdota2.com/matches/${item.id}`,
+  const matchData = getMatches();
+  const tournamentData = getTournaments();
+  const [matches, tournaments] = await Promise.all([matchData, tournamentData]);
+  const matchesMap = matches.map((item) => ({
+    url: `https://www.untilcnwin.com/matches/${item.id}`,
     lastModified: item.updatedAt,
     changeFrequency: 'yearly',
     priority: 0.6,
     alternates: {
       languages: {
-        en: `https://www.playdota2.com/en/matches/${item.id}`,
+        en: `https://www.untilcnwin.com/en/matches/${item.id}`,
       },
     },
-  }))
-  const tournamentsMap = tournaments.map(item => ({
-    url: `https://www.playdota2.com/tournaments/${item.id}`,
+  }));
+  const tournamentsMap = tournaments.map((item) => ({
+    url: `https://www.untilcnwin.com/tournaments/${item.id}`,
     lastModified: item.updatedAt,
     changeFrequency: 'yearly',
     priority: 0.6,
     alternates: {
       languages: {
-        en: `https://www.playdota2.com/en/tournaments/${item.id}`,
+        en: `https://www.untilcnwin.com/en/tournaments/${item.id}`,
       },
     },
-  }))
+  }));
 
   return [
     {
-      url: 'https://www.playdota2.com',
+      url: 'https://www.untilcnwin.com',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 1,
       alternates: {
         languages: {
-          en: 'https://www.playdota2.com/en',
+          en: 'https://www.untilcnwin.com/en',
         },
       },
     },
     {
-      url: 'https://www.playdota2.com/statistics',
+      url: 'https://www.untilcnwin.com/statistics',
       lastModified: new Date(),
       changeFrequency: 'hourly',
       priority: 0.8,
       alternates: {
         languages: {
-          en: 'https://www.playdota2.com/en/statistics',
+          en: 'https://www.untilcnwin.com/en/statistics',
         },
       },
     },
     {
-      url: 'https://www.playdota2.com/tournaments',
+      url: 'https://www.untilcnwin.com/tournaments',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
       alternates: {
         languages: {
-          en: 'https://www.playdota2.com/en/tournaments',
+          en: 'https://www.untilcnwin.com/en/tournaments',
         },
       },
     },
     ...matchesMap,
     ...tournamentsMap,
-  ]
+  ];
 }
